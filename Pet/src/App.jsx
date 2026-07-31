02604cfa1500461df736514bd515a629fbf3c6e8
Ranjan Yadav <ranjanyadav3124@gmail.com>
2026-07-31 23:24:22 +0530

    1st

--- Pet/src/App.jsx
+++ Pet/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Nav from './Component/Nav'
 import Menu from './Component/Menu/Menu'
+import Hero from './Component/Hero/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
     <>
      <Nav/>
      <Menu/>
+     <Hero/>
     </>
   )
 }
---
+++ Pet/src/Component/Hero/Hero.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Hero.css";
+import pets from "../../assets/hero-img.png";
+
+function Hero() {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <h1>
+          Happy Pets,
+          <br />
+          Happy Lives
+        </h1>
+
+        <p>
+          Premium pet supplies for your
+          <br />
+          furry friends
+        </p>
+
+        <button className="hero-btn">Shop Now</button>
+      </div>
+
+      <div className="hero-image">
+        <img src={pets} alt="Dog and Cat" />
+      </div>
+
+      <div className="paw paw1">🐾</div>
+      <div className="paw paw2">🐾</div>
+      <div className="heart">❤</div>
+    </section>
+  );
+}
+
+export default Hero;
